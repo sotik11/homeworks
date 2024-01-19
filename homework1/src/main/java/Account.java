17b42f0e72c2d49ada49e5eb9976f3ca89098c17
sotik <sotik@sanwell.biz>
2024-01-19 15:55:13 +0200

    - first homework

---
+++ homework1/src/main/java/Account.java
@@ -0,0 +1,97 @@
+import java.util.Objects;
+
+public class Account {
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String birthday;
+    private String gender;
+    private String balance;
+
+
+    public Account(String firstName, String lastName, String country, String birthday, String gender, String balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.balance = balance;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(getFirstName(), account.getFirstName()) && Objects.equals(getLastName(), account.getLastName())
+                && Objects.equals(getCountry(), account.getCountry()) && Objects.equals(getBirthday(), account.getBirthday())
+                && Objects.equals(getGender(), account.getGender()) && Objects.equals(getBalance(), account.getBalance());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getCountry(), getBirthday(), getGender(), getBalance());
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", birthday='" + birthday + '\'' +
+                ", gender='" + gender + '\'' +
+                ", balance='" + balance + '\'' +
+                '}';
+    }
+
+
+}
